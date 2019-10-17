9f4a3fabbd0f48372ced307d1cb2909e787ca224
justinyamberla <55857299+justinyamberla@users.noreply.github.com>
2019-10-17 17:50:51 -0500

    none

---
+++ Itro/02-variables-ts.ts
@@ -0,0 +1,5 @@
+var edad = 400;
+let edad = 400;
+const edad = 30;
+const IVA = 12;
+IVA = 40; 
---
+++ Itro/variables.js
@@ -0,0 +1,21 @@
+
+/* 
+Nombre: Daniel Alvarez
+Fecha de actualizacion: 17/10/19
+Descripcion:Ejemplos de variables primitivas en JS
+*/
+/* 
+Primitivas:
+int     -> Numero entero            Ej:1,2
+char    -> caracter alfanumerico    Ej:a,b
+float   -> Numeros decimales        Ej 1.201239 , 12.01239
+double  -> Numeros decimales ++     Ej:1.219400321490124 , 31.21940032149012403
+string  -> arreglo de char          Ej: hola, nelson
+null    -> vacio                    Ej: nulo
+boolean -> Verdadero o falso        Ej: true, false
+*/
+var edad = 400;
+let jola = 400;
+const hola = 30;
+// const IVA = 12;
+// IVA = 40; //no reasignar valores
