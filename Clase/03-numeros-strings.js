dfad688fa0dcc36a413ef4e954117685f3e4ce75
Jenny Llano <55715205+JennyL00@users.noreply.github.com>
2019-10-23 17:59:02 -0500

    hola

---
+++ Clase/03-numeros-strings.js
@@ -0,0 +1,11 @@
+var numeroUno = 1; //camelCase
+// NUMEROUNO    -> TODO MAYUSCULA SIN SEPARACION
+// NUMERO_UNO   -> Mayusculas, guion bajo
+// NumeroUno    -> primeras mayusculas
+// numerouno    -> todas minusculas
+// numero_uno   -> minusculas separacion con guion
+// numerOunO    -> ultima letra mayuscula
+//suma
+var sumaUno = 1 + 2;
+var sumaDos = Math.log10(10);
+console.log(sumaDos);
---
+++ Clase/03-numeros-strings.ts
@@ -0,0 +1,15 @@
+let numeroUno = 1; //camelCase
+
+// NUMEROUNO    -> TODO MAYUSCULA SIN SEPARACION
+// NUMERO_UNO   -> Mayusculas, guion bajo
+// NumeroUno    -> primeras mayusculas
+// numerouno    -> todas minusculas
+// numero_uno   -> minusculas separacion con guion
+// numerOunO    -> ultima letra mayuscula
+
+//suma
+let sumaUno = 1+2;
+let sumaDos = Math.SQRT2;
+console.log(sumaDos);
+
+
