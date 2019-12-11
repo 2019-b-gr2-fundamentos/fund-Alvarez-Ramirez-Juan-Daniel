60147b6c42f819b97a49d79224ce03e43bd6236b
Daniel Alvarez <55715973+DanielAlvarez02@users.noreply.github.com>
2019-12-11 00:36:55 -0500

    arreglo arreglado
    equis de

--- Deber/arreglo_de_vectores.rb
+++ Deber/arreglo_de_vectores.rb
@@ -1,6 +1,6 @@
 def imprimir(array)
-    for i in 0..array.length do
-        puts "dato: "array[i], " ,posicion: ", i 
+    for i in 0..array.length-1 do
+        puts "dato: #{array[i]} ,posicion: #{i}"
     end
     return
 end
@@ -12,37 +12,48 @@ def actualizar(array)
     array.insert(posicion,temp)
     puts "arreglo actualizado"
     imprimir(array)
-    return
+    return array
 end
 def crear(array)
-    puts "ingrese un valor a agregar"
-    temp = gets
-    array.insert(array.length(),temp)
-    return
+    puts "ingrese el tamaño del arreglo (ej: 4)"
+    longitud = gets.to_i
+    for i in 0..longitud-1 do
+      puts "ingrese un valor a agregar"
+      temp = gets.chomp
+      array[i] = temp
+    end
+    imprimir(array)
+    return array
 end
 def eliminar(array)
     puts "ingrese la posicion a eliminar"
-    posicion = gets
-    for i in posicion..array.length do
+    posicion = gets.to_i
+    for i in posicion..array.length-1 do
         array[i] = array[i+1]
     end
     array.pop
-    return
+    imprimir(array)
+    return array
 end
 def main()
-    array = []
-    puts "escoja una opcion crear[1], eliminar elemento[2] o actualizar[3]"
-    opcion = gets
-    es_crear = opcion == "1" || opcion == "crear[1]" || opcion == "crear"  
+    puts "escoja una opcion crear[1], eliminar elemento[2], actualizar[3] o imprimir arreglo[4]"
+    opcion = gets.chomp
+    es_crear = opcion == "1" || opcion == "crear[1]" || opcion == "crear"
     es_eliminar = opcion == "2" || opcion == "eliminar elemento[2]" || opcion == "eliminar elemento"
-    es_actualizar = opcion == "3" || opcion == "actualizar[3]" || opcion == "actualizar" 
+    es_actualizar = opcion == "3" || opcion == "actualizar[3]" || opcion == "actualizar"
+    es_imprimir = opcion == "4" || opcion == "imprimir arreglo[4]" || opcion == "imprimir arreglo"
     if es_crear
-        crear(array)
+      arreglo = crear(arreglo)
     end
-    if es_elemento
-
+    if es_eliminar
+      arreglo = eliminar(arreglo)
     end
     if es_actualizar
+      arreglo = actualizar(arreglo)
     end
+    if es_imprimir
+      imprimir(arreglo)
+    end
+    main
 end
-    
+main
