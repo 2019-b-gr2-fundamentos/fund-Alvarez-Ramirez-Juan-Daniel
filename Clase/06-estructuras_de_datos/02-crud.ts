7f3a3dc5f337c48bc2012c282914afc86c85bd7e
Daniela Romero <daniela.romero@epn.edu.ec>
2020-01-20 15:42:41 -0500

    0
    0

--- Clase/06-estructuras_de_datos/02-crud.ts
+++ Clase/06-estructuras_de_datos/02-crud.ts
@@ -40,42 +40,26 @@ const promesaEdad = prompts({
         console.log('error',error);
     }
 );}
-function obtenerDatosDeAnimalPerritoSincrona(){
-    console.log("Start");
-    
-    const promesaEdad = prompts({
-        type:'number', 
-        name: 'edad',
-        message: 'puedes darme tu edad?'
-    })
-    .then(
-        (datos) => {
-            console.log('datos',datos);
-            
-            const promesaNombre = prompts({
-                type:'text', 
-                name: 'nombre',
-                message: 'puedes darme tu nombre?'
-            })
-            .then(
-                (datos) => {
-                    console.log('datos',datos);
-                
-                }
-            )
-            .catch(
-                (error) => {
-                    console.log('error',error);
-                }
-            );
-        }
-    )
-    .catch(
-        (error) => {
-            console.log('error',error);
-        }
-    );
-    
-console.log("End");
-
+async function obtenerDatosDeAnimalPerritoSincrona(){
+    console.log("Inicio");
+    const preguntas =[
+        {
+            type:'number',
+            name: 'edad',
+            message: 'Puedes darme tu edad?'
+        },
+        {
+            type:'string',
+            name: 'nombre',
+            message: 'Puedes darme tu nombre?'
+        },
+        {
+            type:'string',
+            name: 'cedula',
+            message: 'Puedes darme tu cedula?'
+        },
+    ]
+    const respuestaEdad = await prompts(preguntas);
+    console.log('Respuesta',respuestaEdad)
+    console.log('Fin');
 }
