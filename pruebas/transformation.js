057e64ab1c434f28a9806642b9f445addae50fa7
Daniela Romero <daniela.romero@epn.edu.ec>
2020-01-27 15:54:59 -0500

    0
    0

--- pruebas/transformation.js
+++ pruebas/transformation.js
@@ -2,8 +2,8 @@
 exports.__esModule = true;
 var fs = require("./node_modules/fs-extra");
 var numeroDeIntentosInicial = 10;
-for (var numeroDeIntentos = 1; numeroDeIntentos < numeroDeIntentosInicial; numeroDeIntentos++) {
-    var numberImage = Math.floor(numeroDeIntentos / numeroDeIntentosInicial * 6);
+for (var numeroDeIntentos = 0; numeroDeIntentos < numeroDeIntentosInicial; numeroDeIntentos++) {
+    var numberImage = Math.floor((numeroDeIntentos / numeroDeIntentosInicial) * 6 + 1);
     var imagenAhorcado = fs.readFileSync('./ahorcado_imagenes/' + numberImage + '.txt', 'utf8');
     console.log(imagenAhorcado);
 }
--- pruebas/transformation.ts
+++ pruebas/transformation.ts
@@ -1,12 +1,12 @@
 import * as fs from './node_modules/fs-extra';
 
 const numeroDeIntentosInicial = 10
-for (let numeroDeIntentos = 1; numeroDeIntentos < numeroDeIntentosInicial; numeroDeIntentos++) {
+for (let numeroDeIntentos = 0; numeroDeIntentos < numeroDeIntentosInicial; numeroDeIntentos++) {
 
-    const numberImage = Math.floor(numeroDeIntentos/numeroDeIntentosInicial*6);
+    const numberImage = Math.floor((numeroDeIntentos/numeroDeIntentosInicial)*6+1);
     let imagenAhorcado = fs.readFileSync('./ahorcado_imagenes/'+numberImage+'.txt','utf8');
     console.log(imagenAhorcado); 
-    
+
 }
 
 
