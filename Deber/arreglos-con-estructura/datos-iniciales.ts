cdbe23b7d57657d042e92aef4603ca4a06471b32
Daniela Romero <daniela.romero@epn.edu.ec>
2020-02-03 15:51:51 -0500

    0
    0

--- Deber/arreglos-con-estructura/datos-iniciales.ts
+++ Deber/arreglos-con-estructura/datos-iniciales.ts
@@ -1,4 +1,4 @@
-import * as fs from {fs_extra}
+import * as fs from {'fs-extra'}
 
 export function(){
     const palabrasIniciales:string = fs.readFileSync('C:/Users/USREPS/Documents/GitHub/fund-Alvarez-Ramirez-Juan-Daniel/Deber/arreglos-con-estructura/datos-iniciales/datos-iniciales.txt').toString();
